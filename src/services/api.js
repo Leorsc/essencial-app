8cf3bb568443601977222daa2772c3cb0159e8c1
Leonardo Cruz <112659759+Leorsc@users.noreply.github.com>
2023-03-28 20:32:13 -0300

    Api configurada

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'https://essencial-data-base.herokuapp.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
